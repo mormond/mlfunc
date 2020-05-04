5aae6ebd64773bf37d0a2b2ba4a54b51a22d3c21
Mike Ormond <mormond@microsoft.com>
2020-05-04 17:31:15 +0100

    Update post deploy script

--- Deployment/funcdeploy.sh
+++ Deployment/funcdeploy.sh
@@ -6,3 +6,4 @@ echo 'hello' $PARAM1
 #Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+az functionapp deployment source config-zip -g test1 -n funcappqzyw7bgokigew --src ../requirements.zip
