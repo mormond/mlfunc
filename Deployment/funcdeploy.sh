31d9b33a6782b97ea150353c9d16b03a884debb5
Mike Ormond <mormond@microsoft.com>
2020-05-05 12:07:33 +0100

    Update bash script for debug

--- Deployment/funcdeploy.sh
+++ Deployment/funcdeploy.sh
@@ -9,11 +9,21 @@ FUNCAPPNAME=$5
 echo $USER $TENANT $RG $FUNCAPPNAME
 
 #Install Azure CLI
+echo 'Install Azure CLI'
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+echo '<<<DONE>>> Install Azure CLI'
 
 #Grab python app package
+echo 'Download Python app package'
 curl  https://meobucket.blob.core.windows.net/morescripts/requirements.zip >> requirements.zip
+echo '<<<DONE>>> Download Python app package'
 
+echo 'Azure login'
 az login --service-principal -u $USER -p $PASS -t $TENANT
+echo '<<<DONE>>> Azure login'
 
+echo 'Deploy function app'
 az functionapp deployment source config-zip -g $RG -n $FUNCAPPNAME --src ./requirements.zip
+echo '<<<DONE>>> Deploy function app'
+
+echo 'Done'
