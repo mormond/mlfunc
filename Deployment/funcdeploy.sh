2f86604ec0e9a13271d492dafcc5efc02939947d
Mike Ormond <mormond@microsoft.com>
2020-05-05 11:37:09 +0100

    Update bash script

--- Deployment/funcdeploy.sh
+++ Deployment/funcdeploy.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-PARAM1=$1
-echo 'hello' $PARAM1
+USER=$1 
+PASS=$2 
+TENANT=$3
+RG=$4
+FUNCAPPNAME=$5
+
+echo $USER $TENANT $RG $FUNCAPPNAME
 
 #Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-#az functionapp deployment source config-zip -g test1 -n funcappqzyw7bgokigew --src ../requirements.zip
+#Grab python app package
+curl  https://meobucket.blob.core.windows.net/morescripts/requirements.zip >> requirements.zip
+
+az login --service-principal -u $USER -p $PASS -t $TENANT
+
+az functionapp deployment source config-zip -g $RG -n $FUNCAPPNAME --src ./requirements.zip
